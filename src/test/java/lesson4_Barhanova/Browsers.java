3f9da77adcb0cd3c002d1c8c9d44bc780085d1ea
Rassokhina21 <lekalarina@mail.ru>
2020-02-16 15:39:35 +0300

    Добавлен logger.info

--- src/test/java/lesson4_Barhanova/Browsers.java
+++ src/test/java/lesson4_Barhanova/Browsers.java
@@ -1,15 +1,20 @@
 package lesson4_Barhanova;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 import org.testng.annotations.Test;
 
 public class Browsers {
+    private static final Logger logger = LogManager.getLogger(Browsers.class);
 
-        @Test
+
+    @Test
         public static void main(String[] args) {
             WebDriver wd = WebDriverFactory.createNewDriver("chrome");
             wd.get("https://otus.ru/");
+            logger.info("WebDriverFactory успешно открыл браузер");
             wd.quit();
         }
     }
--- src/test/java/lesson4_Barhanova/WebDriverFactory.java
+++ src/test/java/lesson4_Barhanova/WebDriverFactory.java
@@ -1,28 +1,36 @@
 package lesson4_Barhanova;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverFactory {
+    private static final Logger logger = LogManager.getLogger(WebDriverFactory.class);
+
 
     public static WebDriver createNewDriver(String nameDriver){
         WebDriver driver = null;
         if("FIREFOX".equalsIgnoreCase(nameDriver)){
             WebDriverManager.firefoxdriver().setup();
             driver= new FirefoxDriver();
+            logger.info("Параметром выбран браузер FireFox");
+
         }
         else if("CHROME".equalsIgnoreCase(nameDriver)){
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
+            logger.info("Параметром выбран браузер Chrome");
         }
         else if("EDGE".equalsIgnoreCase(nameDriver)){
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
+            logger.info("Параметром выбран браузер EDGE");
         }else {
-            throw new RuntimeException("Невалидный параметр типа брауера");
+            throw new RuntimeException("Невалидный параметр типа браузера");
         }
         return driver;
 
